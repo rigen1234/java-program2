ada1459ae13ee0d21f885a9f2d9000d3a764f490
Rigen Das <146725937+rigen1234@users.noreply.github.com>
2024-05-23 21:40:23 +0600

    Add files via upload

---
+++ Parameter.java
@@ -0,0 +1,11 @@
+public class Parameter {
+    public static void main(String[] args) {
+        int num=sumTwoNumbers(10,20);
+        System.out.println(num);
+    }
+    public static int sumTwoNumbers(int first,int second){
+        int sum=first + second;
+        return sum;
+    }
+    
+}
