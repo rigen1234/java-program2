0fb90512e6e3303ce3734fec406964857a7850fa
Rigen Das <146725937+rigen1234@users.noreply.github.com>
2024-05-23 21:10:40 +0600

    Add files via upload

---
+++ Function.java
@@ -0,0 +1,27 @@
+public class Function {
+    public static void main(String[] args) {
+        printFirstPattern();
+        System.out.println("\n");
+        printSecondPattern();
+    }
+    public static void  printFirstPattern(){
+        
+        System.out.println("            *");
+        System.out.println("          * *");
+        System.out.println("        * * *");
+        System.out.println("      * * * *");
+        System.out.println("    * * * * *");
+        System.out.println("  * * * * * *");
+        System.out.println("* * * * * * *");
+    }
+    public static void printSecondPattern(){
+    System.out.println("            *");
+    System.out.println("          * *");
+    System.out.println("        * * *");
+    System.out.println("      * * * *");
+    System.out.println("    * * * * *");
+    System.out.println("  * * * * * *");
+    System.out.println("* * * * * * *");
+
+}
+}
