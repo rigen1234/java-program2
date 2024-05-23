9d4e428550d7d301059bc412e4adfd9a57ce75a1
Rigen Das <146725937+rigen1234@users.noreply.github.com>
2024-05-23 22:50:31 +0600

    Add files via upload

---
+++ OddSum.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class OddSum {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Welcome to Odd Sum!\n");
+        System.out.print("Enter any number: ");
+        int num=input.nextInt();
+        int sum=Oddsum(num);
+        System.err.println("Odd Sum till " +num+ " is:" +sum);
+
+
+    }
+    public static int Oddsum(int num){
+        int sum=0;
+        int i=1;
+        while (i<=num) {
+            sum+=i;
+            i+=2;
+            
+        }
+        return sum;
+
+    }
+    
+}
