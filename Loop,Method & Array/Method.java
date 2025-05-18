7a2c01b6c33fd1b4949b364f3e8481e4818f33cf
Rigen Das <146725937+rigen1234@users.noreply.github.com>
2025-05-18 09:25:09 +0600

    Add files via upload

---
+++ Loop,Method & Array/Method.java
@@ -0,0 +1,11 @@
+public class Method {
+    public static void main(String[] args) {
+        System.out.println("Good Morning");
+        first();
+        System.out.println("Method calling complete");
+    }
+    public static void first(){
+        System.out.println("How are you?");
+    }
+    
+}
