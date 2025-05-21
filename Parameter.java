70c9c64e1770e34d3bec892b6f643e968de31fc9
Rigen Das <146725937+rigen1234@users.noreply.github.com>
2025-05-21 21:54:33 +0600

    Add files via upload

--- Parameter.java
+++ Parameter.java
@@ -1,11 +1,16 @@
 public class Parameter {
     public static void main(String[] args) {
-        int num=sumTwoNumbers(10,20);
-        System.out.println(num);
+        System.out.println("The summation of number is : " + sumTwoNumber(6, 9));
+        System.out.println("The summation of number is : " + sumTwoNumber(8, 7));
+        System.out.println("The summation of number is : " + sumTwoNumber(2, 4));
+        
     }
-    public static int sumTwoNumbers(int first,int second){
-        int sum=first + second;
+    public static int sumTwoNumber(int x,int y){
+        System.out.println("First Number Recived : " + x);
+        System.out.println("Second Number Recived :" + y);
+        int sum = x + y;
         return sum;
+
     }
     
 }
